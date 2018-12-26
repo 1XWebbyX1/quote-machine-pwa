93e0f1044979bb17a08e7f580f4ea0e1c9e5fe8c
Khayati Asrani <devinc@TheDen-3.local>
2018-12-27 02:37:10 +0530

    Updates

---
+++ precache-manifest.103e2a92285ea19e496441ff2ee06678.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "763fb42ef6dec0125f94",
+    "url": "/quote-machine-pwa/static/css/main.98b8c0ba.chunk.css"
+  },
+  {
+    "revision": "763fb42ef6dec0125f94",
+    "url": "/quote-machine-pwa/static/js/main.763fb42e.chunk.js"
+  },
+  {
+    "revision": "ddc03ad656f0c6b19c85",
+    "url": "/quote-machine-pwa/static/js/1.ddc03ad6.chunk.js"
+  },
+  {
+    "revision": "46fcc09becaa19db095c",
+    "url": "/quote-machine-pwa/static/js/2.46fcc09b.chunk.js"
+  },
+  {
+    "revision": "e4b2c906687b614dd434",
+    "url": "/quote-machine-pwa/static/js/3.e4b2c906.chunk.js"
+  },
+  {
+    "revision": "e88dbce3451dd3f98e22",
+    "url": "/quote-machine-pwa/static/js/4.e88dbce3.chunk.js"
+  },
+  {
+    "revision": "4bb308f7e6224fe314ae",
+    "url": "/quote-machine-pwa/static/js/5.4bb308f7.chunk.js"
+  },
+  {
+    "revision": "7a425a165b5550510045",
+    "url": "/quote-machine-pwa/static/js/runtime~main.7a425a16.js"
+  },
+  {
+    "revision": "774a92b4eed409bd6f39ca5d6f050594",
+    "url": "/quote-machine-pwa/index.html"
+  }
+];
